27b0dda2443f698f578d77e9e7032fdc02896a40
Arash Nase <ashl7developer@gmail.com>
2025-06-18 20:55:01 -0400

    update dimens

--- android-app-example/app/src/main/java/com/ashl7/designtokens/ui/theme/StyleDictionaryDimension.kt
+++ android-app-example/app/src/main/java/com/ashl7/designtokens/ui/theme/StyleDictionaryDimension.kt
@@ -10,21 +10,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.*
 
 object StyleDictionaryDimension {
-  val dimensions0 = 0.00.dp
-  val dimensions1 = 64.00.dp
-  val dimensions10 = 640.00.dp
-  val dimensions11 = 704.00.dp
-  val dimensions12 = 768.00.dp
-  val dimensions13 = 832.00.dp
-  val dimensions14 = 896.00.dp
-  val dimensions15 = 960.00.dp
-  val dimensions2 = 128.00.dp
-  val dimensions3 = 192.00.dp
-  val dimensions4 = 256.00.dp
-  val dimensions5 = 320.00.dp
-  val dimensions6 = 384.00.dp
-  val dimensions7 = 448.00.dp
-  val dimensions8 = 512.00.dp
-  val dimensions9 = 576.00.dp
-  val dimensionsMax = 159984.00.dp
+  val dimensionsIconSizeLarge = 2048.00.dp
+  val dimensionsIconSizeMedium = 1024.00.dp
+  val dimensionsIconSizeSmall = 512.00.dp
+  val dimensionsRadius0 = 0.00.dp
+  val dimensionsRadius16 = 256.00.dp
+  val dimensionsRadius8 = 128.00.dp
 }
--- mac-os-app-example/mac-os-app-example/generated-tokens/StyleDictionaryDimension.swift
+++ mac-os-app-example/mac-os-app-example/generated-tokens/StyleDictionaryDimension.swift
@@ -9,21 +9,10 @@
 import UIKit
 
 public enum StyleDictionaryDimension {
-    public static let dimensions0 = CGFloat(0.00)
-    public static let dimensions1 = CGFloat(64.00)
-    public static let dimensions10 = CGFloat(640.00)
-    public static let dimensions11 = CGFloat(704.00)
-    public static let dimensions12 = CGFloat(768.00)
-    public static let dimensions13 = CGFloat(832.00)
-    public static let dimensions14 = CGFloat(896.00)
-    public static let dimensions15 = CGFloat(960.00)
-    public static let dimensions2 = CGFloat(128.00)
-    public static let dimensions3 = CGFloat(192.00)
-    public static let dimensions4 = CGFloat(256.00)
-    public static let dimensions5 = CGFloat(320.00)
-    public static let dimensions6 = CGFloat(384.00)
-    public static let dimensions7 = CGFloat(448.00)
-    public static let dimensions8 = CGFloat(512.00)
-    public static let dimensions9 = CGFloat(576.00)
-    public static let dimensionsMax = CGFloat(159984.00)
+    public static let dimensionsIconSizeLarge = CGFloat(2048.00)
+    public static let dimensionsIconSizeMedium = CGFloat(1024.00)
+    public static let dimensionsIconSizeSmall = CGFloat(512.00)
+    public static let dimensionsRadius0 = CGFloat(0.00)
+    public static let dimensionsRadius16 = CGFloat(256.00)
+    public static let dimensionsRadius8 = CGFloat(128.00)
 }
